d1d5bafc473e4381a34596d24fc393e55c754792
JonathasTelesdeOliviera <jonathasteles777@gmail.com>
2026-02-26 13:35:13 -0300

    Finalizando refratoração tratando erros login

--- src/main/java/JonathasTelesdeOliveira/usuario/controller/GlobolExeptionHandler.java
+++ src/main/java/JonathasTelesdeOliveira/usuario/controller/GlobolExeptionHandler.java
@@ -3,22 +3,38 @@ package JonathasTelesdeOliveira.usuario.controller;
 import JonathasTelesdeOliveira.usuario.ifraestruture.exceptions.ConflictException;
 import JonathasTelesdeOliveira.usuario.ifraestruture.exceptions.ResourceNotFoundException;
 import JonathasTelesdeOliveira.usuario.ifraestruture.exceptions.UnauthorizedException;
+import JonathasTelesdeOliveira.usuario.ifraestruture.exceptions.dto.ErrorResponseDTO;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.time.LocalDateTime;
+
 @ControllerAdvice
 public class GlobolExeptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex){
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorResponseDTO> handleResourceNotFoundException(ResourceNotFoundException ex,
+                                                                            HttpServletRequest request) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(buildError(
+                HttpStatus.NOT_FOUND.value(),
+                ex.getMessage(),
+                request.getRequestURI(),
+                "Resource Not Found")
+        );
     }
 
     @ExceptionHandler(ConflictException.class)
-    public ResponseEntity<String> handleConflictException(ConflictException ex){
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    public ResponseEntity<ErrorResponseDTO> handleConflictException(ConflictException ex,
+                                                          HttpServletRequest request){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(buildError(
+                HttpStatus.CONFLICT.value(),
+                ex.getMessage(),
+                request.getRequestURI(),
+                "Conflict Not found")
+        );
     }
 
     @ExceptionHandler(UnauthorizedException.class)
@@ -31,5 +47,14 @@ public class GlobolExeptionHandler {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
+    private ErrorResponseDTO buildError(int Status, String mensage, String path, String error) {
+        return ErrorResponseDTO.builder()
+                .timestamp(LocalDateTime.now())
+                .status(Status)
+                .message(mensage)
+                .error(error)
+                .path(path)
+                .build();
+    }
     
 }
